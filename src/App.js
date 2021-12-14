ccd90e09ff9f66f648af8ebc5ce64eff747d835b
60734740 <jukka.aula@kolumbus.fi>
2021-12-14 18:07:54 +0200

    Valmis

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -16,14 +18,28 @@ function App() {
     fetchItems();
   }, [])
 
+  const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) => 
+    Object.defineProperty(item, 'id', {value: keys[index]}));
+    setTodos(valueKeys);
+  }
 
   const fetchItems = () => {
     fetch('https://react-bookstore-omnia-default-rtdb.europe-west1.firebasedatabase.app/items/.json')
     .then(response => response.json())
-    .then(data => setTodos(Object.values(data)))
+    .then(data => addKeys(data))
     .catch(err => console.error(err))
   }
 
+  const deleteTodo = (id) => {
+    fetch(`https://react-bookstore-omnia-default-rtdb.europe-west1.firebasedatabase.app/items/${id}.json`,
+   {
+      method: 'DELETE',
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
 
   const addTodo = (newTodo) => {
     fetch('https://react-bookstore-omnia-default-rtdb.europe-west1.firebasedatabase.app/items/.json',
@@ -45,11 +61,21 @@ function App() {
         </Toolbar>
       </AppBar> 
       <AddTodo addTodo={addTodo}/> 
-      <div className="ag-theme-material" style={ { height: 400, width: 600, margin: 'auto' } }>
+      <div className="ag-theme-material" style={ { height: 400, width: 700, margin: 'auto' } }>
         <AgGridReact rowData={todos}>
           <AgGridColumn sortable={true} filter={true} field='description' />
           <AgGridColumn sortable={true} filter={true} field='date' />
           <AgGridColumn sortable={true} filter={true} field='priority' />
+          <AgGridColumn 
+            headerName=''
+            field='id' 
+            width={90}
+            cellRendererFramework={ params => 
+              <IconButton onClick={() => deleteTodo(params.value)} size="small" color="secondary">
+                <DeleteIcon />
+              </IconButton>
+            }
+          />      
         </AgGridReact>
       </div>
     </div>
